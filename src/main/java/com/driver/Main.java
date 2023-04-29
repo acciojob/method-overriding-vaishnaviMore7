6681c63dcd3d41da2fad22f5668ea2e71780cc8b
Vaishnavi Sunil More <vaishnavimore9553@gmail.com>
2023-04-29 14:42:42 +0530

    Assignment Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+   public static class B extends A{
+
+    }
+    public static void main(String args[]){
+        B obj=new B();
+        String s=obj.meth();
+        System.out.println(s);
+
+    }
   
 }
