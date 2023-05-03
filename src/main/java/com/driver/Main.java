577e7de1b0fe03c0b82bc5b1ef4fb7f44d47b967
Vaishnavi Sunil More <vaishnavimore9553@gmail.com>
2023-05-03 20:43:54 +0530

    Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,7 +7,9 @@ public class Main {
         }
     }
    public static class B extends A{
-
+public String meth(){
+    return "Method is overridden in Extended class B";
+}
     }
     public static void main(String args[]){
         B obj=new B();
